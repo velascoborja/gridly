57d4dc99337afdaf122b370ba15ad42ad32ac155
borjavg <velasb3@medtronic.com>
2026-04-24 09:50:04 +0200

    Declare smooth scroll behavior

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -60,6 +60,7 @@ export default async function RootLayout({
   return (
     <html 
       lang={locale}
+      data-scroll-behavior="smooth"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-dvh bg-background text-foreground">
